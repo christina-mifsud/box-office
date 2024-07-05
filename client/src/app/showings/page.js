f1abe5a8f1bf17d91e44ed47125bbd6a6b505c5b
christina-mifsud <christina.mifsud@gmail.com>
2024-07-05 20:42:18 +0100

    fetching data from json

--- client/src/app/showings/page.js
+++ client/src/app/showings/page.js
@@ -1,8 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import SingleMovieCard from "../components/SingleMovieCard";
+
 export default function Showings() {
+  // single card for movie which will then be mapped over
+  // fetch data useEffect
+  // useState to handle state change & to show different ui
+  const [movieArray, setMovieArray] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("http://localhost:9090/api/shows/all-open")
+        .then((response) => response.json())
+        .then((data) => setMovieArray(data))
+        .catch((error) => console.error(error));
+    };
+    fetchData();
+  }, []);
+
+  const temp = "string";
+  // console.log(movieArray.showings);
+  // movie objects < showings array < movieArray object
+
+  const showingsArray = movieArray.showings;
+
   return (
     <main>
-      <h1>Now showing</h1>
-      <p>List the theatre showings here...</p>
+      <h1>Showings page</h1>
+      <SingleMovieCard showingsArray={showingsArray} />
     </main>
   );
 }
